7fe806a659d6abc7e35582ff1ec3aa10033d288d
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-18 13:58:12 +0000

    Updated main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void _prints(char *s);
+void print_int(int n);
+int _printf(const char *format, ...);
+
+#endif
