af72067dfd29327f71c1b73504b146e3d19296dc
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-20 16:44:47 +0000

    Updated files

--- main.h
+++ main.h
@@ -9,9 +9,9 @@
 
 int _putchar(char c);
 int (*choice(char s))(va_list);
-void print_int(va_list args);
+int print_int(va_list args);
 int _printf(const char *format, ...);
 int print_c(va_list args);
-int print_s(va_list args)
+int print_s(va_list args);
 
 #endif
